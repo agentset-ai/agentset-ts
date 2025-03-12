b9bf19bc7cd6041b205892162fab9565da39c366
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-13 01:36:02 +0200

    :rocket: initial commit

---
+++ tooling/prettier/index.js
@@ -0,0 +1,24 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@agentset",
+    "^@agentset/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+};
+
+export default config;
