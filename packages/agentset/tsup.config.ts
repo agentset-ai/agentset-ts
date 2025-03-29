a16e78733a37d30bb64b6b3c536cd303a7ed275d
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-03-29 09:02:39 +0200

    update build config

--- packages/agentset/tsup.config.ts
+++ packages/agentset/tsup.config.ts
@@ -3,20 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig(() => ({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  outExtension({ format }) {
-    const ext = format === "esm" ? "mjs" : "js";
-    const finalFormat = format === "cjs" || format === "esm" ? "" : format;
-
-    const outputExtension = `${finalFormat}.${ext}`;
-
-    return {
-      js: outputExtension.startsWith(".")
-        ? outputExtension
-        : `.${outputExtension}`,
-    };
-  },
-  treeshake: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
